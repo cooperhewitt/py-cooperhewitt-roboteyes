f6e7500647d9b4a517fb9c7bc815d611a0b47b69
copea <copea@si.edu>
2014-11-06 15:50:40 -0500

    install all the other packages

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup
 
 setup(name='cooperhewitt-roboteyes',
       version='0.1',
-      description='Simple computer vision related functions',
+      description='Meta package to install all the Cooper Hewitt computer vision libraries',
       author='Cooper Hewitt Smithsonian Design Museum',
       url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes',
-      requires=[],
-      packages=[
-          'cooperhewitt',
-          'cooperhewitt.roboteyes'
-      ],
+      requires=[
+        'cooperhewitt.roboteyes.atkinson'
+        'cooperhewitt.roboteyes.shannon',
+        'cooperhewitt.roboteyes.opencv'
+        ],
+      packages=[]
       scripts=[],
       download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes/releases/tag/v0.1',
       license='BSD')
