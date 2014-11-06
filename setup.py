7f40a0493a3231be8cea5a3110673389c6e4e592
straup <github@aaronland.net>
2014-11-06 08:15:35 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cooperhewitt-roboteyes',
+      version='0.1',
+      description='Simple computer vision related functions',
+      author='Cooper Hewitt Smithsonian Design Museum',
+      url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes',
+      requires=[],
+      packages=[
+          'cooperhewitt',
+          'cooperhewitt.roboteyes'
+      ],
+      scripts=[],
+      download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes/releases/tag/v0.1',
+      license='BSD')
