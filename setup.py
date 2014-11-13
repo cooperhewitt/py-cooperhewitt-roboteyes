4c3d92d603f416c20bca863021b751e9422adb05
straup <straup@gmail.com>
2014-11-13 15:03:11 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='cooperhewitt-roboteyes',
-      version='0.1',
-      description='Meta package to install all the Cooper Hewitt computer vision libraries',
-      author='Cooper Hewitt Smithsonian Design Museum',
-      url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes',
-      requires=[
+setup(
+    name='cooperhewitt.roboteyes',
+    version='0.1',
+    description='Meta package to install all the Cooper Hewitt computer vision libraries',
+    author='Cooper Hewitt Smithsonian Design Museum',
+    url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes',
+    install_requires=[
         'cooperhewitt.roboteyes.atkinson'
         'cooperhewitt.roboteyes.shannon',
         'cooperhewitt.roboteyes.opencv'
-        ],
-      packages=[]
-      scripts=[],
-      download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes/releases/tag/v0.1',
-      license='BSD')
+    ],
+      dependency_links=[
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson/archive/v0.1.tar.gz#egg=cooperhewitt.roboteyes.atkinson-0.1',
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-shannon/archive/v0.1.tar.gz#egg=cooperhewitt.roboteyes.shannon-0.1',
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-opencv/archive/v0.1.tar.gz#egg=cooperhewitt.roboteyes.opencv-0.21',
+      ],
+    packages=[]
+    scripts=[],
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes/releases/tag/v0.1',
+    license='BSD')
