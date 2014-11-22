a06e8612f94f69a324ae5968fbe4d5516f01310a
straup <straup@gmail.com>
2014-11-22 14:46:24 +0100

    fix urls; add -colors

--- setup.py
+++ setup.py
@@ -4,21 +4,23 @@ from setuptools import setup
 
 setup(
     name='cooperhewitt.roboteyes',
-    version='0.1',
+    version='0.11',
     description='Meta package to install all the Cooper Hewitt computer vision libraries',
     author='Cooper Hewitt Smithsonian Design Museum',
     url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes',
     install_requires=[
-        'cooperhewitt.roboteyes.atkinson'
+        'cooperhewitt.roboteyes.atkinson',
+        'cooperhewitt.roboteyes.colors',
+        'cooperhewitt.roboteyes.opencv',
         'cooperhewitt.roboteyes.shannon',
-        'cooperhewitt.roboteyes.opencv'
     ],
       dependency_links=[
-          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson/archive/v0.1.tar.gz#egg=cooperhewitt.roboteyes.atkinson-0.1',
-          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-shannon/archive/v0.1.tar.gz#egg=cooperhewitt.roboteyes.shannon-0.1',
-          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-opencv/archive/v0.1.tar.gz#egg=cooperhewitt.roboteyes.opencv-0.21',
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson/tarball/master#egg=cooperhewitt.roboteyes.atkinson-0.1',
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-colors/tarball/master#egg=cooperhewitt.roboteyes.colors-0.1',
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-opencv/tarball/master#egg=cooperhewitt.roboteyes.opencv-0.21',
+          'https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-shannon/tarball/master#egg=cooperhewitt.roboteyes.shannon-0.1',
       ],
     packages=[]
     scripts=[],
-    download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes/releases/tag/v0.1',
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes/releases/tag/v0.11',
     license='BSD')
